0572f68b0008e67fb75187d2eccd6f5f8fe045d5
rekriz11 <rekriz@seas.upenn.edu>
2022-07-23 16:30:08 -0400

    Update generation_utils.py

--- src/transformers/generation_utils.py
+++ src/transformers/generation_utils.py
@@ -1663,7 +1663,7 @@ class GenerationMixin:
                         ## When a non-overlapping previous candidate is found, mark it as used context
                         if context[i:i+len(prev)].tolist() == prev and \
                         used_context[i:i+len(prev)] == [0 for j in range(len(prev))]:
-                            used_context[i:i+len(prev)] == [1 for j in range(len(prev))]
+                            used_context[i:i+len(prev)] = [1 for j in range(len(prev))]
                             print("Found!, used context updated: {}".format(used_context))
                             found = True
                             break
