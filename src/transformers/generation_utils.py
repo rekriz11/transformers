67baf638aea1985a04e8a0b6445be5edf6a8d4df
rekriz11 <rekriz@seas.upenn.edu>
2022-08-15 15:26:22 -0400

    Update generation_utils.py

--- src/transformers/generation_utils.py
+++ src/transformers/generation_utils.py
@@ -1700,7 +1700,6 @@ class GenerationMixin:
             real_next_token = tokenizer.convert_ids_to_tokens(real_next_id)
             print("Real next id: {}, token: {}, real_score: {}".format(real_next_id, real_next_token, real_score))
             rscores, rids = torch.topk(scores[beam_idx], 5, dim=-1, largest=True, sorted=True)
-            import pdb; pdb.set_trace()
             rscores, rids = [s.item() for s in rscores], [i.item() for i in rids]
             rtokens = tokenizer.convert_ids_to_tokens(rids)
             print("Original top 5:\n{}\n".format("\n".join([str((rids[i], rtokens[i], rscores[i])) for i in range(len(rscores))])))
