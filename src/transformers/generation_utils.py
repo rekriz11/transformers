133cc8baa5c6742660281d0d4511c03f705fc1f1
rekriz11 <rekriz@seas.upenn.edu>
2022-10-04 14:04:44 -0400

    Update generation_utils.py

--- src/transformers/generation_utils.py
+++ src/transformers/generation_utils.py
@@ -2330,7 +2330,7 @@ class GenerationMixin:
             next_token_scores = logits_processor(input_ids, next_token_logits)
             next_token_scores = logits_warper(input_ids, next_token_scores)
             if constrained_type != None and 'entity_input' in constrained_type:
-                if debug_id == '14618_2':
+                if debug_id == '14617_2':
                     print("\n##### STEP {} #####".format(cur_len))
                 next_token_scores = self.set_scores_to_inf_for_invalid_inputs(next_token_scores, input_ids, disjoint_entities, valid_input, empty_answer, delimiters, eos_token_id, constrained_type, tokenizer, debug_id)
 
